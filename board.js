6b9f2f59bfc5cf1f010fbc1a47e0cd892f668929
youngniiiiiii <124110329+youngniiiiiii@users.noreply.github.com>
2023-04-06 17:39:11 +0900

    Add files via upload
    today..

--- board.js
+++ board.js
@@ -1,44 +1,28 @@
-var express = require("express");
-var app = express(); // 서버 만들었음
-var fs = require("fs");
-var ejs = require("ejs");
-//ejs엔진은 views폴더 아래서 파일을 검색한다.
-app.set("view engine", ejs);
-app.use(express.urlencoded({extended:false}));
+var express = require('express');
+var router = express.Router();
+var commonDB = require("./commonDB");
 
-let boardList = [
-    {id:1, title:"제목1", writer:"작성자1", wdate:"2023-04-04"},
-    {id:2, title:"제목2", writer:"작성자2", wdate:"2023-04-05"},
-    {id:3, title:"제목3", writer:"작성자3", wdate:"2023-04-06"},
-    {id:4, title:"제목4", writer:"작성자4", wdate:"2023-04-07"},
-    {id:5, title:"제목5", writer:"작성자5", wdate:"2023-04-08"}
-];
-app.use("/board/list", (request, response) => {
-  response.render("board/board_list.ejs", {boardList:boardList,totalCnt:5});
-});
-app.use("/board/view/:id", (request, response) => {
-  let id = request.params.id;
-  let item = boardList.filter(x=>x.id==id);
-  response.render("board/board_view.ejs", {item:item[0]});
-});
-//페이지만 이동한다. board_write.ejs로 이동만 한다
-app.use("/board/write", (request, response) => {
-  response.render("board/board_write.ejs");
-});
-//저장하기
-app.use("/board/save", (request, response) => {
-  let title = request.body.title;
-  let contents = request.body.contents;
-  let writer = request.body.writer;
-  let id = boardList.length+1;
-  boardList.push({id:id, title:title, contents:contents, writer:writer});
-  response.redirect("/board/list");  //강제이동
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  let sql=`
+      select id, title, writer, 
+      contents, date_format(wdate, '%y-%m-%d')wdate
+      from tb_board
+  `;
+
+  let results = await commonDB.mysqlRead(sql, []);
+  res.render('board/board_list', { boardList: results });
 });
 
-app.use((request, response) => {
-  response.writeHead(200, { "Content-type": "text/html" });
-  response.end("<h1>Express</h1>");
+router.get('/view/:id', async function(req, res, next) {
+  let id = req.params.id;
+  let sql = `select id, title, writer, 
+            contents, date_format(wdate, '%y-%m-%d')wdate
+            from tb_board where id=${id}`;
+  let results = await commonDB.mysqlRead(sql, [id]);
+  res.render('board/board_view', { board: results[0] });
 });
-app.listen(4000, () => {
-  console.log("server start http://127.0.0.1:4000");
-});
+
+
+
+module.exports = router;
---
+++ commonDB.js
@@ -0,0 +1,39 @@
+//board.js에서 디비접근, member.js 디비접근  --디비에 데이터 읽고 쓰기 전문 코드
+
+var mysql = require("mysql");
+const DBInfo={
+    connectionLimit:10,
+    host:"localhost",
+    user:"user01",
+    password:"1234",
+    database:"mydb",
+    port:3306
+};
+
+let readpool = mysql.createPool(DBInfo);        //DB연결하는애야
+async function mysqlRead(sql, params)
+{
+    let promise = new Promise((resolve, reject)=>{
+        readpool.getConnection( (err, conn)=>{
+            if(err)
+            {
+                console.log(err);
+                reject(err);
+            }
+
+            conn.query(sql, params, (err, rows)=>{
+                console.log( sql );
+                console.log( rows );
+                if(err)
+                    reject(err);
+                else
+                    resolve(rows);
+                conn.release();
+            })
+        })
+    });
+    await promise;
+    return promise;
+}
+
+exports.mysqlRead = mysqlRead;      //이거 추가했다 내보내줘야함... 너써도된다 허락권한
